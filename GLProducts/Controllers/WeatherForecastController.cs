e0ec8e879c80ff5a1723567ba5f5d892491a416c
Vibhor Goel <vibhor.goel@soprasteria.com>
2022-06-11 18:19:17 +0530

    Testing the changes

--- GLProducts/Controllers/WeatherForecastController.cs
+++ GLProducts/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace GLProducts.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Jamm gaya", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
