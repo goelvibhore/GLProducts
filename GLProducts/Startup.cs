430d68bc5aa1ea9a7a9de12c7570db7957819e0e
Vibhor Goel <vibhor.goel@soprasteria.com>
2022-06-12 18:24:58 +0530

    Updating code

--- GLProducts/Startup.cs
+++ GLProducts/Startup.cs
@@ -1,7 +1,9 @@
+using GLProducts.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,9 @@ namespace GLProducts
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
             });
+
+            services.AddDbContext<GLContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("SimpleDatabaseManagementDb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
